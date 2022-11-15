c08d6e7110872a4c8a595c4d048a477b70a4a260
taniusa90 <galea_tanea@mail.com>
2022-11-14 21:17:40 -0600

    Security

--- src/main/java/com/cydeo/TicketingProjectSecurityApplication.java
+++ src/main/java/com/cydeo/TicketingProjectSecurityApplication.java
@@ -1,13 +1,26 @@
 package com.cydeo;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class TicketingProjectSecurityApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TicketingProjectSecurityApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper mapper(){
+        return new ModelMapper();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+    return new BCryptPasswordEncoder();//impl of Password interface
     }
 
 }
